cfa76aaf4e6a5b65f7a2d20860082a01dae16e8a
esilver0 <40437745+esilver0@users.noreply.github.com>
2018-08-02 11:50:21 -0400

    Sets up the webpages on the webserver

---
+++ set-homepages.sh
@@ -0,0 +1,7 @@
+cd /var/www/html/  
+
+sudo wget -e robots=off --wait 1 -H -p -k http://engineering.nyu.edu/
+sudo wget -e robots=off --wait 1 -H -p -k http://facebook.com/  
+sudo wget -e robots=off --wait 1 -H -p -k http://youtube.com/  
+sudo wget -e robots=off --wait 1 -H -p -k http://reddit.com/  
+sudo wget -e robots=off --wait 1 -H -p -k http://www.mlb.com/mets 
