c337915e14da3a7c17b9f14d1177ef15973b35a9
Fraida Fund <ffund@nyu.edu>
2020-08-10 21:27:44 -0400

    Update relay-install.sh

--- relay-install.sh
+++ relay-install.sh
@@ -1,3 +1,21 @@
 sudo apt-get update
 sudo apt-get -y --force-yes install tor vim curl tor-arm
 sudo pkill -9 tor
+
+sudo bash -c "cat >/etc/tor/torrc <<EOL
+TestingTorNetwork 1
+DataDirectory /var/lib/tor
+RunAsDaemon 1
+ConnLimit 60
+ShutdownWaitLength 0
+PidFile /var/lib/tor/pid
+Log notice file /var/log/tor/notice.log
+Log info file /var/log/tor/info.log
+Log debug file /var/log/tor/debug.log
+ProtocolWarnings 1
+SafeLogging 0
+DisableDebuggerAttachment 0
+DirAuthority $HOSTNAME orport=5000 no-v2 hs v3ident=$finger1 192.168.1.4:7000 $finger2
+SocksPort 9050
+ControlPort 9051
+EOL"
